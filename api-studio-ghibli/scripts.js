2b736660507bbfd1a2313c00ab73ed29d096837f
Marlon Braga <rdg.brg@gmail.com>
2018-11-29 04:29:31 -0500

    api-studio-ghibli

---
+++ api-studio-ghibli/scripts.js
@@ -0,0 +1,63 @@
+const app = document.getElementById('root');
+
+const logo = document.createElement('img');
+logo.src = 'logo.png';
+
+const container = document.createElement('div');
+container.setAttribute('class', 'container');
+
+app.appendChild(logo);
+app.appendChild(container);
+
+/* <div id="root">
+  <img src="logo.png">
+  <div class="container"></div>)
+</div> */
+// console.log(app);
+
+// Create a request variable and assign a new XMLHttpRequest object to it.
+var request = new XMLHttpRequest();
+
+// Open a new connection, using the GET request on the URL endpoint
+request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
+
+request.onload = function () {
+
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response);
+
+  if (request.status >= 200 && request.status < 400) {
+    data.forEach(movie => {
+      
+      // Create a div with a card class
+      var card = document.createElement('div');
+      card.setAttribute('class', 'card');
+      console.log(this.response)
+
+      // Create an h1 and set the text content to the film's title
+      const h1 = document.createElement('h1');
+      h1.textContent = movie.title;
+
+      // Create a p and set the text content to the film's description
+      const p = document.createElement('p');
+      movie.description = movie.description.substring(0, 300);  // Limit to 300 characters
+      p.textContent = `${movie.description}...`;                // End with an ellipses    
+
+      // Append the cards to the container element
+      container.appendChild(card);
+
+      // Each card will contain an h1 and a p
+      card.appendChild(h1);
+      card.appendChild(p);
+      
+    });
+  } else {
+    const errorMessage = document.createElement('marquee');
+    errorMessage.textContent = `Gah, it's not working, dude!`;
+    app.appendChild(errorMessage);
+  }
+
+}
+
+// Send request
+request.send();
